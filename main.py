26e001b9a1548db5d913387ab94f0071631cb4d0
Deema Rain <deemarain@dhcp-892bfda7.ucd.ie>
2023-11-11 15:07:42 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+def  quadraticFormula1(a,b,c):
+    root1 = (-b + math.sqrt(b**2 -4*a*c))\(2*a)
+    return root1
+
+def quadraticFormula2(a,b,c):
+    root2 = (-b + math.sqrt(b**2 -4*a*c))\(2*a)
+    return root2
+
+#testing
+a= 2
+b= -4
+c= 1
+
